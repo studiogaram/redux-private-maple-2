82f64cfbfd1e2928c1e551b1153dd4f7a4c82c11
garam <studio@gar.am>
2016-06-11 12:49:23 +0900

    Tutorial Todo List on Redux.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './containers/App'
-import configureStore from './store/configureStore'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-const store = configureStore()
+let store = createStore(todoApp)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 )
