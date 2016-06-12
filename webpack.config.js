0daacc3cf565f05a206c168f2cb1b24468601e52
garam <studio@gar.am>
2016-06-12 14:22:08 +0900

    Setting gitignore, index, web pack build path.

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,24 @@
-var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
     './index'
   ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+  output: { path: __dirname, filename: 'bundle.js' },
+  devServer: {
+    inline: true,
+    port: 8080,
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: ['babel'],
-        exclude: /node_modules/,
-        include: __dirname
-      }
-    ]
-  }
-}
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/,
+      query: {
+        cacheDirectory: true,
+        presets: ['es2015', 'react'],
+      },
+    }],
+  },
+};
